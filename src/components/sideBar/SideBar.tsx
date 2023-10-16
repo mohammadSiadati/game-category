cd5c06bb5c6d834548f630ff160e95576cd2c0ff
m.siadati <msiyadati7779@gmail.com>
2023-10-16 16:24:06 +0330

    add cart item and header

---
+++ src/components/sideBar/SideBar.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import List from '@mui/material/List';
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
+import Toolbar from '@mui/material/Toolbar';
+
+const SideBar = () => {
+  return (
+    <Box sx={{background: '#DAFFFB'}}>
+      <Divider />
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+
+    </Box>
+  );
+};
+
+export default SideBar;
